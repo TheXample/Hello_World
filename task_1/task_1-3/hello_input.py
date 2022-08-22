0ce970196c3c2bb089103a727d785d18f56c190e
Hannes <hannes.schniz@gmx.de>
2022-08-22 22:15:47 +0200

    init hello_input

---
+++ task_1/task_1-3/hello_input.py
@@ -0,0 +1,15 @@
+# Aighty, miss superhero. 
+# Now we need to acknowledge, that going into the source code and putting in a password right next
+# to the correct one is not the most secure way to lock our very veluable and precious data.
+# So we will use the console to input our password.
+
+# Please initialize your password variable here again
+
+
+# Now we will make our input variable again. BUT we will use the "input" methode 
+# and safe whatever it gives us in our variable. 
+# You can put text into your methode to tell the person behind the screen what to put in.
+# Please put the variable here
+
+
+#The rest of the programm is the same. You can just write the same as the other task underneath here.
