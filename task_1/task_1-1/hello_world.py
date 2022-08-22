471694a16eeea51d92168217ec7ab3b2dfd81fa9
Hannes <hannes.schniz@gmx.de>
2022-08-22 22:14:19 +0200

    init first three tasks

---
+++ task_1/task_1-1/hello_world.py
@@ -0,0 +1,7 @@
+# Hi baby, its your time to shine. 
+# Your'e my super hero so well figure out now how to say hello to the world!
+
+
+# To tell the world hello, we will use our trusty printer. luykily its already built in to your computer.
+# Your printer methode will take care of that, you just need to call it. 
+# You do so like this: methode(input) thats it
---
+++ task_1/task_1-2/hello_if.py
@@ -0,0 +1,21 @@
+# Hi again baby, well now make your hello call a little more secure by adding a password.
+
+# Imagine this being your super hero computer, first you need to think of a password and write in the line underneath to remember it.
+# myPassword: 
+
+# Then well use a variable to safe it for your computer. 
+# We do that by finding a name for it and then writing it like this: name = value
+# Now its your turn in the line underneath you can put your variable with your password.
+
+
+# Okay now well make another variable for the input just the way we did the other one underneath here.
+
+
+# Great! Now we need a way to compare those two passwords. We'll do that by using the "if" methode.
+# You have seen how to use a methode in the last task so now well do it again. 
+# An "if" gets a statement, and if that statement is true it'll do the things that are indented after it, if not itll not.
+# As a statement you have "<,>,==,!=" choose your fighter!
+# Write your if in the line underneath
+
+
+# Alright we have made it to here now we just need to hit tab once to make sure the code will only be started if our statement is true.
