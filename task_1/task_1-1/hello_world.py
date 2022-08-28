e49546b163a3f53de8b13709c6403cdd8eed7e1c
Hannes <hannes.schniz@gmx.de>
2022-08-29 01:15:32 +0200

    working on task 1_3 solved task 1_1 and1_2 Co-authored-by: Starz25 <Starz25@users.noreply.github.com>

--- task_1/task_1-1/hello_world.py
+++ task_1/task_1-1/hello_world.py
@@ -1,7 +1,8 @@
 # Hi baby, its your time to shine. 
-# Your'e my super hero so well figure out now how to say hello to the world!
+# You're my super hero so well figure out now how to say hello to the world!
 
+# To tell the world hello, we will use our trusty printer. luckily its already built in to your computer.
+# Your printer method will take care of that, you just need to call it. 
+# You do so like this: method(input) thats it
 
-# To tell the world hello, we will use our trusty printer. luykily its already built in to your computer.
-# Your printer methode will take care of that, you just need to call it. 
-# You do so like this: methode(input) thats it
+print("ILOVEYOU<3")
--- task_1/task_1-3/hello_input.py
+++ task_1/task_1-3/hello_input.py
@@ -1,15 +1,16 @@
+
 # Aighty, miss superhero. 
 # Now we need to acknowledge, that going into the source code and putting in a password right next
-# to the correct one is not the most secure way to lock our very veluable and precious data.
+# to the correct one is not the most secure way to lock our very valuable and precious data.
 # So we will use the console to input our password.
 
 # Please initialize your password variable here again
-
+password = "DADDY"
 
 # Now we will make our input variable again. BUT we will use the "input" methode 
 # and safe whatever it gives us in our variable. 
 # You can put text into your methode to tell the person behind the screen what to put in.
 # Please put the variable here
-
+input("put in ur password pls UWU: ")
 
 # The rest of the programm is the same. You can just write the same as the other task underneath here.
