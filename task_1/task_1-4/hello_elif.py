4a9e0bd284daacb3222a5e50b30b477e2f1a5e8f
Hannes <hannes.schniz@gmx.de>
2022-08-29 01:25:40 +0200

    made another task

---
+++ task_1/task_1-4/hello_elif.py
@@ -0,0 +1,12 @@
+# Welcome back wonder baby. This time around well see two different ways to distinguish between inputs.
+# First up well need an input. We learned how that works in the last task so your first quest is to make a variable and store the input there.
+
+
+# Wonderful! Now we'll see the first thing we can do with our if. We want to make it so the program stopps when the User says "stop" or "Stop".
+# So we'll need to check if our input variable is one of those two. 
+# To do that we use the equals again from task 1.2 and behind that another equals and separate them with an "OR".
+# Now if eather of those statements is correct it will do what is indented behind it.
+
+
+# Now we want to make a little Menu. so if the user pressed the 1 it says hello. If the user pressed 2 it says Bye.
+# We can do that by using the "elif" (else if) function. 
